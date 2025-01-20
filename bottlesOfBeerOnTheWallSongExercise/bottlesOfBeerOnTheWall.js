fecb61b4777c6174a22836704965e8345be9836a
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-01-21 02:17:16 +0800

    with a for loop

--- bottlesOfBeerOnTheWallSongExercise/bottlesOfBeerOnTheWall.js
+++ bottlesOfBeerOnTheWallSongExercise/bottlesOfBeerOnTheWall.js
@@ -1,44 +1,30 @@
 
 // let numberOfBottles = 99;
+// function beer(){
 
-// function beer() {
-//     while (numberOfBottles > 0) {
-//         if (numberOfBottles > 1) {
-//             console.log(`${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer. Take one down, pass it around, ${numberOfBottles - 1} bottles of beer on the wall.`);
-//         } else {
-//             console.log(`${numberOfBottles} bottle of beer on the wall, ${numberOfBottles} bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.`);
-//         }
-//         numberOfBottles--;
+
+// while(numberOfBottles > 0){
+//     if(numberOfBottles > 1){
+//         console.log(`${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer. Take one down, pass it around, ${numberOfBottles - 1} bottles of beer on the wall.`);
+//     } else{
+//         console.log(`${numberOfBottles} bottle of beer on the wall, ${numberOfBottles} bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.`);
+//     } 
+//     numberOfBottles--;
 //     }
-//     console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.`);
+//     console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, no more bottles of beer on the wall.`);
 // }
 
 // beer();
-
-
-
-
-
-
-
-
-
-
-
-
-let numberOfBottles = 99;
 function beer(){
-
-
-while(numberOfBottles > 0){
+for(let numberOfBottles = 99; numberOfBottles>0; numberOfBottles--){
     if(numberOfBottles > 1){
         console.log(`${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer. Take one down, pass it around, ${numberOfBottles - 1} bottles of beer on the wall.`);
     } else{
         console.log(`${numberOfBottles} bottle of beer on the wall, ${numberOfBottles} bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.`);
-    } 
-    numberOfBottles--;
     }
-    console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.`);
+    
+}
+console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, no more bottles of beer on the wall.`);
 }
 
 beer();
